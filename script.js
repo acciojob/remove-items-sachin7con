d65fa822adb8ce013ab5989740cdb3e1a29f7b71
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-21 14:05:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// SGN your JS code here. If required.
+let remColor = document.getElementById("colorSelect");
+function removeSelectedOption() {
+    let selectedIndex = remColor.selectedIndex;
+    if (selectedIndex !== -1) {
+        remColor.options[selectedIndex].remove();
+    }
+}
